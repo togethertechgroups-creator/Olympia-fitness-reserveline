68c023293457dd5ab3626aaf16319cad9a122846
Aarsha <aarshak2004@gmail.com>
2026-08-10 17:57:01 +0530

    Reduce charts height to equal the height of recent transactions feed

--- src/components/PtRevenueChart.jsx
+++ src/components/PtRevenueChart.jsx
@@ -29,7 +29,7 @@ const PtRevenueChart = ({ data = [] }) => {
           </div>
         ) : (
           <ResponsiveContainer width="100%" height="100%">
-            <BarChart data={data} margin={{ top: 20, right: 20, left: 0, bottom: 20 }}>
+            <BarChart data={data} margin={{ top: 10, right: 15, left: 0, bottom: 10 }}>
               <XAxis 
                 dataKey="name" 
                 axisLine={false} 
